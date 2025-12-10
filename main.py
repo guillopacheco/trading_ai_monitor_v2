d6b3a1ca6178975a838f7b8ed7db01ed92d4d042
guillopacheco <guillopacheco@gmail.com>
2025-12-10 16:46:05 -0500

    main

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
 import logging
-from logger_config import configure_logging
-from database import init_db
+
+from telegram.ext import Application
+from config import TELEGRAM_BOT_TOKEN
+from services.telegram_service.notifier import Notifier
 from application_layer import ApplicationLayer
-from services.telegram_service.command_bot import start_command_bot
-from services.telegram_service.telegram_reader import start_telegram_reader
-from services.signals_service.signal_reactivation_sync import start_reactivation_monitor
+from services.telegram_service.command_bot import CommandBot
 
 logger = logging.getLogger("MAIN")
 
@@ -16,17 +16,9 @@ async def main():
     notifier = Notifier(application.bot)
     app_layer = ApplicationLayer(notifier)
 
-    command_bot = CommandBot(app_layer)
+    command_bot = CommandBot(app_layer, application)
     command_bot.run()
-
-    # Mantener bot vivo
-    await bot_app.updater.wait_for_stop()
-
-    logger.info("🛑 Servicios cerrados.")
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except Exception as e:
-        logger.error(f"❌ Error crítico: {e}", exc_info=True)
+    asyncio.run(main())
