04ea9ed77d689420702dd72a25d909f21b209a70
guillopacheco <guillopacheco@gmail.com>
2025-12-14 03:18:41 -0500

    app

--- application_layer.py
+++ application_layer.py
@@ -1,41 +1,28 @@
 # application_layer.py
 
-import logging
-
-from services.telegram_service.notifier import Notifier
 from services.application.analysis_service import AnalysisService
 from services.application.signal_service import SignalService
-
-from services.reactivation_engine.reactivation_engine import ReactivationEngine
+from services.application.operation_service import OperationService
 from services.coordinators.signal_coordinator import SignalCoordinator
+from services.telegram_service.notifier import Notifier
 
-logger = logging.getLogger("application_layer")
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class ApplicationLayer:
-    """
-    Contenedor de dependencias (DI simple).
-    Construye servicios y coordinadores con firmas estables.
-    """
-
-    def __init__(self, bot):
-        self.bot = bot
-
-        # Notifier (PTB bot)
-        self.notifier = Notifier(bot)
-
-        # Services
-        self.analysis_service = AnalysisService()
+    def __init__(self):
+        # Servicios base
+        self.analysis = AnalysisService()
         self.signal_service = SignalService()
+        self.operation = OperationService()
+        self.notifier = Notifier()
 
-        # Engines
-        self.reactivation_engine = ReactivationEngine()
-
-        # Coordinators
+        # Coordinadores (usan servicios)
         self.signal = SignalCoordinator(
+            analysis=self.analysis,
             signal_service=self.signal_service,
-            analysis_service=self.analysis_service,
-            reactivation_engine=self.reactivation_engine,
             notifier=self.notifier,
         )
 
